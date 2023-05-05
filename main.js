6d1bff0692ffa747e3912dc4176e342181f0ddd6
jyotiprajapti <jyotiprajapati3029@gmail.com>
2023-05-05 08:27:55 +0530

    both files added

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("welcome to emplyee wage distribution")
+let x = Math.random() * 1;
+if(x>1.5){
+    console.log('employee is present')
+}else{
+    console.log('employe is absent')
+}
+ let wagePerHour = 20
+ let fullWorkingHours = 8
+ const dailyWage = wagePerHour*fullWorkingHours
+ console.log(dailyWage)
+let partTimehour = 4;
+const partimeWage = wagePerHour*partTimehour
+console.log(partimeWage)
+let working_days_in_month = 20
+let wage_in_month = working_days_in_month*dailyWage
+console.log(wage_in_month)
